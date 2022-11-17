d033c3ef20743ec1a1ffc6aa764a20d466947de9
S.Naito <na.shota.1139@gmail.com>
2022-11-17 22:32:51 +0900

    :wrench: chore: NextUIの導入

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
+import { NextUIProvider } from '@nextui-org/react'
+
 import type { AppProps } from 'next/app'
 
+import { Layout } from 'components/layouts/Layout'
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <NextUIProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </NextUIProvider>
+  )
 }
 
 export default MyApp
---
+++ client/pages/_document.js
@@ -0,0 +1,26 @@
+import { CssBaseline } from '@nextui-org/react'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return {
+      ...initialProps,
+      styles: <>{initialProps.styles}</>,
+    }
+  }
+
+  render() {
+    return (
+      <Html lang='en'>
+        <Head>{CssBaseline.flush()}</Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
