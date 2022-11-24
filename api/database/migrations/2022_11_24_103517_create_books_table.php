90246f4c8604e107fa234800ff92a09de167634e
S.Naito <na.shota.1139@gmail.com>
2022-11-24 21:24:50 +0900

    :sparkles: feat: 本関連のmigrationファイル作成

---
+++ api/database/migrations/2022_11_24_103517_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string("title")->unique();
+            $table->string("image_url");
+            $table->integer("page");
+            $table->date('published_date');
+            $table->foreignId('author_id')->constrained('authors');
+            $table->foreignId('publisher_id')->constrained('publishers');
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
