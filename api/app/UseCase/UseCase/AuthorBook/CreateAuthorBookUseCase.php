af3a3095a6fde4306a6c1187dccfd193b3036a63
S.Naito <na.shota.1139@gmail.com>
2022-12-11 22:45:17 +0900

    :sparkles: Commandで本関連の情報を作成できるように

---
+++ api/app/UseCase/UseCase/AuthorBook/CreateAuthorBookUseCase.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Usecase\Usecase\AuthorBook;
+
+use App\Domain\DomainService\AuthorBookDomainService;
+use App\Domain\DTO\AuthorBookDto;
+use App\Domain\Entity\AuthorBookEntity;
+use App\Domain\RepositoryInterface\AuthorBookRepositoryInterface;
+
+class CreateAuthorBookUseCase
+{
+    private AuthorBookRepositoryInterface $authorBookRepository;
+    private AuthorBookDomainService $authorBookDomainService;
+
+    public function __construct(
+        AuthorBookRepositoryInterface $authorBookRepository,
+        AuthorBookDomainService $authorBookDomainService,
+    ) {
+        $this->authorBookRepository = $authorBookRepository;
+        $this->authorBookDomainService = $authorBookDomainService;
+    }
+
+    /**
+     * @param AuthorBookEntity $authorBookEntity
+     * @return AuthorBookDto
+     */
+    public function execute(AuthorBookEntity $authorBookEntity): AuthorBookDto
+    {
+        if ($this->authorBookDomainService->Exists($authorBookEntity)) {
+            return $this->authorBookRepository->getByAuthorIdBookId($authorBookEntity->authorId(), $authorBookEntity->bookId());
+        }
+        return $this->authorBookRepository->save($authorBookEntity);
+    }
+}
---
+++ api/database/migrations/2022_12_11_091109_create_author_book_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('author_book', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('author_id')->nullable()->constrained('authors');
+            $table->foreignId('book_id')->constrained('books');
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('author_book');
+    }
+};
