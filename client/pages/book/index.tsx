627e3b716b9ddb0d009c3fcfb797b9bcc7d7f05e
S.Naito <na.shota.1139@gmail.com>
2022-12-18 22:54:30 +0900

    :recycle: 本のCardをComponentに切り出し

--- client/pages/book/index.tsx
+++ client/pages/book/index.tsx
@@ -1,10 +1,9 @@
-import { Grid, Card, Col, Row } from '@nextui-org/react'
-
-import { Button94f9f0 } from '../../components/elements/Button/Button94f9f0'
+import { Grid } from '@nextui-org/react'
 
 import type { GetServerSideProps, NextPage } from 'next'
 
-import { bookIndexType, fetchBookType } from 'features/book/types'
+import { BookImageCard } from 'components/elements/Card/BookImageCard'
+import { BookIndexType, FetchBookType } from 'features/book/types'
 import { axios } from 'libs/axios'
 
 export const getServerSideProps: GetServerSideProps = async () => {
@@ -16,48 +15,13 @@ export const getServerSideProps: GetServerSideProps = async () => {
   }
 }
 
-const Book: NextPage<bookIndexType> = ({ books }) => {
+const Book: NextPage<BookIndexType> = ({ books }) => {
   return (
     <Grid.Container gap={4} justify='center'>
-      {books.map((book: fetchBookType, index: number) => {
+      {books.map((book: FetchBookType, index: number) => {
         return (
-          <Grid xs={3} key={index}>
-            <Card css={{ w: '100%', h: 'auto' }}>
-              <Card.Body css={{ p: 0 }}>
-                <Card.Image
-                  src={book.cover_image_url}
-                  objectFit='cover'
-                  width='100%'
-                  height='100%'
-                  alt='Relaxing app background'
-                />
-              </Card.Body>
-              <Card.Footer
-                isBlurred
-                css={{
-                  position: 'absolute',
-                  bgBlur: '#0f111466',
-                  borderTop: '$borderWeights$light solid $gray800',
-                  bottom: 0,
-                  zIndex: 1,
-                }}
-              >
-                <Row>
-                  <Col>
-                    <Row>
-                      <Col>
-                        <div className={'text-xs text-slate-900'}>{book.title}</div>
-                      </Col>
-                    </Row>
-                  </Col>
-                  <Col>
-                    <Row justify='flex-end'>
-                      <Button94f9f0 text={'Detail'} url={`/books/${book.id}`} />
-                    </Row>
-                  </Col>
-                </Row>
-              </Card.Footer>
-            </Card>
+          <Grid xs={2} key={index}>
+            <BookImageCard book={book} />
           </Grid>
         )
       })}
