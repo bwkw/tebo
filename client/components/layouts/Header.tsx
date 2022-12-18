7c40757fe9cf6838a157aaa1b5c752219dba1183
S.Naito <na.shota.1139@gmail.com>
2022-12-18 22:06:20 +0900

    :+1: Headerが固定されるように機能改善

--- client/components/layouts/Header.tsx
+++ client/components/layouts/Header.tsx
@@ -12,7 +12,7 @@ export const Header = () => {
   ]
 
   return (
-    <nav className='flex bg-black bg-opacity-90 sticky top-0 px-2 sm:px-4 py-2.5 rounded'>
+    <nav className='flex bg-black bg-opacity-90 sticky top-0 px-2 sm:px-4 py-2.5 rounded z-50'>
       <div className='container flex flex-wrap items-center justify-between mx-auto'>
         <Link href='/'>
           <a className='flex items-center'>
@@ -21,11 +21,11 @@ export const Header = () => {
           </a>
         </Link>
         <div className='hidden w-full md:block md:w-auto' id='navbar-default'>
-          <ul className='flex flex-col p-4 mt-4 border rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0'>
+          <ul className='flex flex-col p-5 items-center border rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0'>
             {isLogin ? (
               <>
                 {loginMenuLists.map((value, index) => (
-                  <li key={index} className='text-white block py-2 pl-3 pr-4 rounded md:bg-transparent md:p-0'>
+                  <li key={index} className='text-white block pl-3 pr-4 rounded md:bg-transparent md:p-0'>
                     <Link href={value.link}>{value.name}</Link>
                   </li>
                 ))}
