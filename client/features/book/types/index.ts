aff5bf734c3f1e17996585877fcf915c25f0e009
S.Naito <na.shota.1139@gmail.com>
2022-12-18 16:41:38 +0900

    :sparkles: ssr実装

---
+++ client/features/book/types/index.ts
@@ -0,0 +1,12 @@
+export type fetchBookType = {
+  id: number
+  title: string
+  description: string
+  cover_image_url: string
+  page: number
+  published_date: Date
+  created_at: Date
+  updated_at: Date
+  authors: string[]
+  publisher: string
+}
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,6 +1,18 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 
-const Home: NextPage = () => {
+import { fetchBookType } from 'features/book/types'
+import { axios } from 'libs/axios'
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  // const fetcher = (url: string): Promise<fetchBookType[]> => axios.get(url).then((res) => res.data)
+  // const { data: books, error } = useSWR('/api/books', fetcher)
+  const books = await axios.get('/api/books').then((res) => res.data)
+
+  return { props: { books } }
+}
+
+const Home: NextPage<fetchBookType> = (books) => {
+  console.log(books)
   return <div className='underline'>テスト</div>
 }
 
