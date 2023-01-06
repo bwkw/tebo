c878146b98e9c8aca6f44e44d9743bbf65fdeef4
S.Naito <na.shota.1139@gmail.com>
2023-01-06 23:10:50 +0900

    :sparkles: usersテーブル定義

--- api/database/migrations/2014_10_12_000000_create_users_table.php
+++ api/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->datetime('email_verified_at');
             $table->string('password');
             $table->rememberToken();
-            $table->string('icon');
+            $table->string('icon')->nullable();
             $table->dateTime('created_at');
             $table->dateTime('updated_at');
         });
