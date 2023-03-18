f22ea0b6e0a7963c7a003fc548c45daa7b7ae357
bwkw <na.shota.1139@gmail.com>
2023-03-18 04:26:56 -0700

    :recycle: extract api process to retrieve books

--- api/app/Application/UseCase/API/FetchBooksByGoogleBooksApiUseCase.php
+++ api/app/Application/UseCase/API/FetchBooksByGoogleBooksApiUseCase.php
@@ -2,48 +2,24 @@
 
 namespace App\Application\UseCase\API;
 
-use App\Application\UseCase\Author\CreateAuthorUseCase;
-use App\Application\UseCase\AuthorBook\CreateAuthorBookUseCase;
-use App\Application\UseCase\Book\CreateBookUseCase;
-use App\Application\UseCase\Publisher\CreatePublisherUseCase;
-use App\Domain\Author\AuthorEntity;
-use App\Domain\AuthorBook\AuthorBookEntity;
-use App\Domain\Book\BookEntity;
-use App\Domain\Publisher\PublisherEntity;
 use Carbon\CarbonImmutable;
 
-class fetchBooksByGoogleBooksApiUseCase
+class FetchBooksByGoogleBooksApiUseCase
 {
-    private CreateAuthorUseCase $createAuthorUseCase;
-    private CreateBookUseCase $createBookUseCase;
-    private CreatePublisherUseCase $createPublisherUseCase;
-    private CreateAuthorBookUseCase $createAuthorBookUseCase;
-
-    public function __construct(
-        CreateAuthorUseCase $createAuthorUseCase,
-        CreateAuthorBookUseCase $createAuthorBookUseCase,
-        CreateBookUseCase $createBookUseCase,
-        CreatePublisherUseCase $createPublisherUseCase,
-    ) {
-        $this->createAuthorUseCase = $createAuthorUseCase;
-        $this->createAuthorBookUseCase = $createAuthorBookUseCase;
-        $this->createBookUseCase = $createBookUseCase;
-        $this->createPublisherUseCase = $createPublisherUseCase;
-    }
-
     /**
      * @param string $keyword
      * @param int $count
+     * @return array
      */
-    public function execute(string $keyword, int $count): void
+    public function execute(string $keyword, int $count): array
     {
         $maxResults = 10;
         $googleBooksApiBaseUrl = 'https://www.googleapis.com/books/v1/volumes';
         $openBdApiBaseUrl = 'https://api.openbd.jp/v1/get?isbn=';
         $googleBooksApiBaseUrl .= '?q=' . $keyword . '&maxResults=' . $maxResults;
 
+        $books = [];
         $iterationCount = floor($count / $maxResults);
-
         for ($i = 0; $i <= $iterationCount; $i++) {
             $startIndex = $maxResults * $i; // $startIndexは0始まり
             $googleBooksApiUrl = $googleBooksApiBaseUrl . '&startIndex=' . $startIndex;
@@ -70,13 +46,6 @@ class fetchBooksByGoogleBooksApiUseCase
                 }
 
                 $publisher = $result->volumeInfo->publisher ?? null;
-                if ($publisher) {
-                    $publisherEntity = PublisherEntity::constructNewInstance($publisher);
-                    $publisherDto = $this->createPublisherUseCase->execute($publisherEntity);
-                    $publisherId = $publisherDto->id;
-                } else {
-                    $publisherId = null;
-                }
                 $title = $result->volumeInfo->title;
                 $description = $result->volumeInfo->description ?? ""; //説明はGoogleBooksApiで十分そう
                 $coverImageUrl = $summary->cover ?? "";
@@ -84,42 +53,24 @@ class fetchBooksByGoogleBooksApiUseCase
                 $publishedDate =
                     new CarbonImmutable($result->volumeInfo->publishedDate) ??
                     new CarbonImmutable("99991231");
-                $authors = $result->volumeInfo->authors ?? null; //著者はGoogleBooksApiの方がデータ持ってる
-                if ($authors) {
-                    foreach ($authors as $author) {
-                        $authorEntity = AuthorEntity::constructNewInstance($author);
-                        $authorDto = $this->createAuthorUseCase->execute($authorEntity);
-                        $bookEntity = BookEntity::constructNewInstance(
-                            $title,
-                            $description,
-                            $coverImageUrl,
-                            $page,
-                            $publishedDate,
-                            $publisherId,
-                        );
-                        $bookDto = $this->createBookUseCase->execute($bookEntity);
-                        $authorBookEntity = AuthorBookEntity::constructNewInstance(
-                            $authorDto->id,
-                            $bookDto->id,
-                        );
-                        $this->createAuthorBookUseCase->execute($authorBookEntity);
-                    }
-                } else {
-                    $authorId = null;
-                    $bookEntity = BookEntity::constructNewInstance(
-                        $title,
-                        $description,
-                        $coverImageUrl,
-                        $page,
-                        $publishedDate,
-                        $publisherId,
-                    );
-                    $bookDto = $this->createBookUseCase->execute($bookEntity);
-                    $authorBookEntity = AuthorBookEntity::constructNewInstance($authorId, $bookDto->id);
-                    $this->createAuthorBookUseCase->execute($authorBookEntity);
+                $authors = $result->volumeInfo->authors ?? [null]; //著者はGoogleBooksApiの方がデータ持ってる
+
+                foreach ($authors as $author) {
+                    $book = [
+                        'title' => $title,
+                        'description' => $description,
+                        'coverImageUrl' => $coverImageUrl,
+                        'page' => $page,
+                        'publishedDate' => $publishedDate,
+                        'publisher' => $publisher,
+                        'author' => $author
+                    ];
+                    $books[] = $book;
                 }
             }
             sleep(2);
         }
+
+        return $books;
     }
 }
